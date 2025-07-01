c356cce02c15100b0941fad7dea0b63a0616e8fa
romumupointparis <romubuntu@gmail.com>
2025-07-01 19:13:38 +0200

    fix: wrong types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { AVATAR_ELEMENTS } from "./contants";
 import PlayfulAvatar from "./PlayfulAvatar";
 import PlayfulElement from "./PlayfulElement";
-import { type TPlayfulAvatarProps, TUserConfig, TAvatarConfig } from "./types";
+import { type TPlayfulAvatarProps, TUserConfig, TAvatarConfig, TAvatarAccessory, TAvatarBackgroundShape, TAvatarElement,TAvatarElements,TAvatarEyes,TAvatarFace,TAvatarHaircut,TAvatarMouth,TAvatarOutfit} from "./types";
 import { genUserConfigBySeed } from "./utils/avatarUtils";
 
 export { PlayfulAvatar, PlayfulElement, AVATAR_ELEMENTS };
 export {genUserConfigBySeed};
-export { type TPlayfulAvatarProps, type TUserConfig, type TAvatarConfig};
+export type {TPlayfulAvatarProps, TUserConfig, TAvatarConfig, TAvatarAccessory, TAvatarBackgroundShape, TAvatarElement, TAvatarElements, TAvatarEyes, TAvatarFace, TAvatarHaircut, TAvatarMouth, TAvatarOutfit };
